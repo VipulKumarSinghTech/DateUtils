c18b00bce5e94363d64934b0b08547d9dab3ebef
Vipul Kumar Singh <vipul24.9669@gmail.com>
2021-10-02 18:57:30 +0530

    Added LocalDate to Date Conversion with ZoneId

--- src/main/java/com/github/VipulKumarSinghTech/DateUtils.java
+++ src/main/java/com/github/VipulKumarSinghTech/DateUtils.java
@@ -1,7 +1,15 @@
 package com.github.VipulKumarSinghTech;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
 public class DateUtils {
 
     private DateUtils() {
+    }
+
+    public static Date convertToDate(LocalDate localDate, ZoneId zoneId) {
+        return Date.from(localDate.atStartOfDay(zoneId).toInstant());
     }
 }
